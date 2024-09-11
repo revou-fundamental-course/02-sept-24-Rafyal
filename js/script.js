d7de5da7d58c6db662e87eaee91ec3d1e2852c99
Rafyal <rafyal991@gmail.com>
2024-09-11 22:10:27 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-IniJavaScript
+
+function validateLuas() {
+
+let panjangAlas = document.getElementById('panjang-alas').value;
+let tinggi = document.getElementById('tinggi').value;
+console.log(panjangAlas);
+console.log(tinggi);
+}
