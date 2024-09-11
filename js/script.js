c8f512c2b8a5f24d853b9ef19f120ce558264c88
Rafyal <rafyal991@gmail.com>
2024-09-12 01:33:00 +0700

    UPDATE 2

--- js/script.js
+++ js/script.js
@@ -6,3 +6,20 @@ let tinggi = document.getElementById('tinggi').value;
 console.log(panjangAlas);
 console.log(tinggi);
 }
+
+
+function segitiga(evt, cityName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("Build");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " w3-red";
+}
+ 
+
